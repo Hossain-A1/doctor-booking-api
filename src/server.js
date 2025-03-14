e416d8063ff1feea2df5e7f1e5644fa97d1708c0
Hossain-A1 <mrhossainahmed7@gmail.com>
2025-03-14 16:38:57 +0300

    bug fix

--- src/server.js
+++ src/server.js
@@ -1,9 +1,22 @@
-const {PORT} = require('./secret.js')
+const { PORT } = require("./secret.js");
 
-const app = require('./app.js');
-const connectDB = require('./config/db.js');
+const app = require("./app.js");
+const connectDB = require("./config/db.js");
 
-app.listen(PORT, async()=>{
-  console.log(`App running on port ${PORT}`);
-await connectDB()
-})
+const startServer = async () => {
+  try {
+    await connectDB();
+    console.log("Database connections established");
+  } catch (error) {
+    console.error("Failed to establish connections", error);
+  }
+};
+
+startServer();
+
+// Conditionally listen to the server in development mode
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+  });
+}
